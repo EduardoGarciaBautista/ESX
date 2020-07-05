6c8e0ad2d18f9eaca1217afe6fa39be9919c4708
eduardo <egarciab@ids.com.mx>
2020-07-04 21:37:09 -0500

    [ADD] Se agrega ejemplo de es7 que salio en junio del 2016 con includes y como elevar a la potencia

---
+++ src/es7/index.js
@@ -0,0 +1,20 @@
+// nace en junio del 2016 y se agregan 
+// includes trabaja sobre un array o string y 
+// permite saber si existe un elemento 
+// tambien se agrego como elevar a la potencia
+// cons **
+
+//includes
+let numbers = [1,2,3,4,5,6,7];
+
+if (numbers.includes(2)) {
+console.log('Existe');
+}
+
+//potencia
+let base = 4;
+let exponent = 5;
+
+let result = base**exponent;
+
+console.log(result);
